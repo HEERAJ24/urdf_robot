678c020f210a931f09eb313033547b893396af48
Heeraj Ayyappan <77336089+HEERAJ24@users.noreply.github.com>
2024-01-11 13:44:15 +0100

    Delete build/urdf_project directory

--- build/urdf_project/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hee/ros2_ws/src/urdf_project
-# Build directory: /home/hee/ros2_ws/build/urdf_project
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
